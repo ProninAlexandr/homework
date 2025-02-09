a9028c846bc94853aea871e66a3df373b2bedbfb
ProninAlexandr <163460617+ProninAlexandr@users.noreply.github.com>
2025-02-10 02:40:16 +0300

    created

---
+++ HW1(go).c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+int main(int argc, char **argv)
+{
+/*
+printf("Hello world!\n");
+*/  
+/*
+	 Let’s
+       go
+			to walk
+*/
+    printf("Let's\n");
+    printf("  go\n");
+    printf("    to walk\n");
+
+
+return 0;
+}

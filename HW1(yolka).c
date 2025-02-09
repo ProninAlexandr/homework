551ec99d5d28d5904991ff8b92db1da0144d1e44
ProninAlexandr <163460617+ProninAlexandr@users.noreply.github.com>
2025-02-10 02:47:47 +0300

    1

---
+++ HW1(yolka).c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+int main(int argc, char **argv)
+{
+/*
+printf("Hello world!\n");
+*/  
+/*
+	 Let’s
+       go
+			to walk
+
+    printf("Let's\n");
+    printf("  go\n");
+    printf("    to walk\n");
+
+*/
+/*
+     *
+    ***
+   *****
+  *******
+   HHOHH
+   ZZZZZ
+*/
+
+printf("   *\n");
+printf("  ***  \n");
+printf(" ***** \n");
+printf("*******\n");
+printf(" HHOHH\n");
+printf(" ZZZZZ\n");
+
+return 0;
+}
